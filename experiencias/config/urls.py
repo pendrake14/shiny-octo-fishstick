d8917cd144603f8904cdb80cf76dce2b7b59fc5d
Vic <victor@cipherxmdr.io>
2025-05-01 13:34:18 +0200

    Update API documentation URLs for Swagger and Redoc

--- experiencias/config/urls.py
+++ experiencias/config/urls.py
@@ -7,8 +7,8 @@ swagger = [
     # YOUR PATTERNS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns = [
